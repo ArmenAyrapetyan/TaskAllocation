9dd293f84ad2a1a427418971e555e88f85a8b243
ArmenAyrapetyan <manho2002@mail.ru>
2022-07-14 12:01:03 +0500

    dev|make migrations

---
+++ app/Models/SpecialGroup.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SpecialGroup extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2022_07_14_000018_create_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->longText('description');
+            $table->integer('time_planned');
+            $table->integer('time_spend')->nullable();
+            $table->date('date_start');
+            $table->integer('date_end');
+            $table->foreignId('project_id')->constrained('projects')->cascadeOnDelete();
+            $table->foreignId('status_id')->constrained('task_statuses')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
